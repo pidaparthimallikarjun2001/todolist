15ea80d0664e434122953cc2506b585d561d0a19
Pidaparthi Mallikarjun <pidaparthimallikarjun1482001@gmail.com>
2020-07-26 17:18:30 +0530

    Updates

---
+++ precache-manifest.2810a3490e8a89d26618f3c4037ad75d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3890026dbe0dc964bebbf10a5038d40",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "58acf03b445065dcedc0",
+    "url": "/todolist/static/css/main.49eff6f6.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/todolist/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58acf03b445065dcedc0",
+    "url": "/todolist/static/js/main.1a61b398.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
